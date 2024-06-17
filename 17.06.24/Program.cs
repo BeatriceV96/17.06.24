b52a7e5dd7b5f57275c97435962b3b6f4ae584c5
BeatriceV96 <beatricevanvitelli1@gmail.com>
2024-06-17 18:22:43 +0200

    Aggiungere i file di progetto.

---
+++ 17.06.24/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _17._06._24
+{
+    internal class Program
+    {
+        class Atleta
+        {
+            string nome = "";
+            string cognome = "";
+            int eta;
+            string sport = "";
+
+            public string Nome { get { return nome; } set { nome = value; } }
+            public string Cognome { get { return cognome; } set { cognome = value; } }
+            
+            public int Eta { get { return eta; } set { eta = value; } }
+
+            public string Sport { get { return sport; } set { sport = value; } }
+
+            public void Descrizione() {
+
+                Console.WriteLine("L'atleta si chiama " + nome + " " + cognome + " ha " + eta + " anni e pratica " + sport);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Atleta atleta = new Atleta();
+            atleta.Nome = "Salvatore";
+            atleta.Cognome = "Rossi";
+            atleta.Eta = 26;
+            atleta.Sport = "Nuoto";
+
+            atleta.Descrizione();
+        }
+    }
+}
